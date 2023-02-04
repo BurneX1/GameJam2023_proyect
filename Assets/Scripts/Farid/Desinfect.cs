4a78bc59abc7147f63bedcbadffe0ea725a7f5d6
Ricardo Farid Sotomayor Cuentas <rifa172004@gmail.com>
2023-02-04 16:41:45 -0500

    Inicio de desinfección de objetos
    El inicio xd.

--- Assets/Scripts/Farid/Desinfect.cs
+++ Assets/Scripts/Farid/Desinfect.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Desinfect : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
