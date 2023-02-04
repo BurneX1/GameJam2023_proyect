4e7b2b95576c8da924b6e5a9e491b98b47866654
Ale_pc_tls <65439485+BrunoSalas@users.noreply.github.com>
2023-02-04 03:08:47 -0500

    first push

---
+++ Assets/Scripts/ScriptableObjects/AttackType.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackType : ScriptableObject
+{
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
